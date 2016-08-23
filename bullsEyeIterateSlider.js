31be369a85383aed3068262c56a9a9f5354cc712
zichenma <mazichen1984@gmail.com>
2016-08-23 12:43:35 -0400

    first commit

---
+++ bullsEyeIterateSlider.js
@@ -0,0 +1,47 @@
+﻿window.onload = init;
+
+// canvas and context variables
+var canvas,context,centerX,centerY;
+
+
+function init() {
+     canvas = document.getElementById("Canvas");
+     context = canvas.getContext("2d");
+
+    // Get the center coordinates of circles
+    centerX = canvas.width / 2;
+    centerY = canvas.height / 2;
+
+    // draw the initial pattern
+    drawPattern();
+    console.log(bandWidth.value);
+}
+
+    function drawPattern() {
+        var radius = 200;
+        var bandWidth = document.getElementById("bandWidth").value;
+        var i = 1;
+        // draw the patterns with loops
+        while (radius > 0) {
+            if (i % 2 == 1) {
+                circlePattern(radius, "red");
+            }
+            else {
+                circlePattern(radius, "blue");
+            }
+            i++;
+            radius -= bandWidth;
+        }
+    }
+
+    //create a pattern function
+    function circlePattern(radius, color) {
+        var radius = radius;
+        context.fillStyle = color;
+        //begin drawing the circle
+        context.beginPath();
+        context.arc(centerX, centerY, radius, 0, 2 * Math.PI);
+        context.fill();
+        context.closePath();
+    }
+
